7e92f4012d9ae90286ba797d07df43412f26242b
DJ-Sparky <Sparky@ravemail.com>
2022-05-19 10:11:10 +0100

    embed video Iframe not thumbnail

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+/* eslint-disable @next/next/no-img-element */
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 function youtubeApi({ endpoint, query }) {
   let searchParams = new URLSearchParams({
     ...query,
     key: process.env.YOUTUBE_API_KEY,
-  });
-  return `https://www.googleapis.com/youtube/v3/${endpoint}?${searchParams.toString()}`;
+  })
+  return `https://www.googleapis.com/youtube/v3/${endpoint}?${searchParams.toString()}`
 }
 
 export async function getServerSideProps(ctx) {
@@ -17,13 +18,13 @@ export async function getServerSideProps(ctx) {
       maxResults: '50',
       playlistId: 'PLurCdizYrXX409OJB1iaxqb236Zu8-opC',
     },
-  });
-  const data = await fetch(link).then((res) => res.json());
-  return { props: { data } };
+  })
+  const data = await fetch(link).then((res) => res.json())
+  return { props: { data } }
 }
 
 export default function Home({ data }) {
-  console.log('Home index.js data is:', data);
+  console.log('Home index.js data is:', data)
   return (
     <div className={styles.container}>
       <Head>
@@ -40,35 +41,36 @@ export default function Home({ data }) {
               thumbnails,
               description = {},
               resourceId = {},
-            } = snippet;
+            } = snippet
 
-            let length = 300;
-            let myTruncatedString = description.substring(0, length);
-            console.log({ myTruncatedString });
+            let length = 300
+            let myTruncatedString = description.substring(0, length)
+            console.log({ myTruncatedString })
 
-            const { medium } = thumbnails;
+            const { medium } = thumbnails
             return (
               <li key={id}>
-                <a
-                  href={`https://www.youtube.com/watch?v=${resourceId.videoId}`}
-                >
-                  <p>
-                    <img
-                      width={medium.width}
-                      height={medium.height}
-                      src={medium.url}
-                      alt={title}
-                    />
-                  </p>
-                  <h3>{title}</h3>
-                  <h4>{myTruncatedString}.. Read more</h4>
-                </a>
+                <p>
+                  <iframe
+                    src={`https://www.youtube.com/embed/${resourceId.videoId}`}
+                    width={medium.width}
+                    height={medium.height}
+                  />
+                  {/* <img
+                    width={medium.width}
+                    height={medium.height}
+                    src={medium.url}
+                    alt={title}
+                  /> */}
+                </p>
+                <h3>{title}</h3>
+                <h4>{myTruncatedString}.. Read more</h4>
               </li>
-            );
+            )
           })}
         </ul>
       </main>
       <footer className={styles.footer}></footer>
     </div>
-  );
+  )
 }
